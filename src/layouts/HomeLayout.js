8f9ff90ac0b2147ad547df375152a0f840d3c569
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-05-25 01:04:41 +0300

    feat: update HomeLayout and its css

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -4,20 +4,25 @@ import '../styles/HomeLayout.css';
 
 const HomeLayout = () => {
   return (
-    <div className='container'>
+    <>
       <header className="header">
-        <h1>Welcome to Beauty Salon!</h1>
-        {/*TODO: Add links to each page */}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/client">Client Account</Link>
+        <h1 className="header__title">Welcome to Beauty Salon!</h1>
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <Link to="" className="header__nav-link">Main</Link>
+            </li>
+            <li className="header__nav-item">
+              <Link to="about" className='header__nav-link'>About</Link>
+            </li>
+            <li className="header__nav-item">
+              <Link to="profile" className='header__nav-link'>Profile</Link>
             </li>
           </ul>
         </nav>
       </header>
       <Outlet />
-    </div>
+    </>
   )
 }
 
