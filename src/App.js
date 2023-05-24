a347cadf026301f681dd74d32934eb90cad0127b
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-05-24 23:11:31 +0300

    feat: update routing and add new components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import './index.css';
 import HomeLayout from './layouts/HomeLayout';
-import AboutPage from './layouts/AboutLayout';
+import AboutLayout from './layouts/AboutLayout';
 import HomePage from './pages/HomePage';
+import Services from './pages/Services';
+import Masters from './pages/Masters';
+import Schedule from './pages/Schedule';
+import AboutPage from './pages/AboutPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomeLayout />} >
       <Route index element={<HomePage />} />
-      <Route path="about" element={<AboutPage />} />
+      <Route path="about" element={<AboutLayout />} >
+        <Route index element={<AboutPage />} />
+        <Route path="services" element={<Services />} />
+        <Route path="masters" element={<Masters />} />
+        <Route path="schedule" element={<Schedule />} />
+      </Route>
     </Route>
   )
 )
---
+++ src/pages/Masters.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Masters = () => {
+  return (
+    <h1>Masters</h1>
+  )
+}
+
+export default Masters
