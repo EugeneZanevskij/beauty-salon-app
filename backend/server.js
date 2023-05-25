f6fa64b28c8b5d2d89984cf7b1535a7fb409f605
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-05-25 22:09:52 +0300

    feat: create something like Schedule

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,28 @@ app.get('/api/masters', (req, res) => {
     res.json(results);
     // res.send(results);
   });
-})
+});
+
+app.get('/api/appointments', (req, res) => {
+  const query = `
+    SELECT a.id, c.firstName, c.lastName, a.date_signup, a.time_signup, m.firstName as masterFirstName, m.lastName as masterLastName, s.name as serviceName
+    FROM client_master_services a
+    INNER JOIN client c ON a.client_id = c.id
+    INNER JOIN master_services ms ON a.master_service_id = ms.id
+    INNER JOIN master m ON ms.master_id = m.id
+    INNER JOIN services s ON ms.service_id = s.id
+    ORDER BY a.date_signup DESC, a.time_signup ASC;
+  `;
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Error retrieving appointments' });
+    }
+
+    res.json(results);
+  });
+});
 
 // Routes
 // app.use('/api', routes);
--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import '../styles/Schedule.css';
+import api from '../api';
 
 const Schedule = () => {
-  return (
-    <h1>Schedule</h1>
-  )
-}
+  const [appointments, setAppointments] = useState([]);
 
-export default Schedule
+  const loadAppointments = async () => {
+    const response = await api.get('/api/appointments');
+    setAppointments(response.data);
+  };
+
+  const formatDate = (date) => {
+    const dateObject = new Date(date);
+    const formattedDate = dateObject.toLocaleDateString();
+    return formattedDate;
+  }
+
+  useEffect(() => {
+    loadAppointments();
+  }, []);
+
+  return (
+    <div className="schedule-page">
+      <h1>Schedule</h1>
+      <table className="schedule-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Client</th>
+            <th>Master</th>
+            <th>Service</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map((appointment) => (
+            <tr key={appointment.id}>
+              <td>{appointment.id}</td>
+              <td>{formatDate(appointment.date_signup)}</td>
+              <td>{appointment.time_signup}</td>
+              <td>{appointment.firstName} {appointment.lastName}</td>
+              <td>{appointment.masterFirstName} {appointment.masterLastName}</td>
+              <td>{appointment.serviceName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Schedule;
