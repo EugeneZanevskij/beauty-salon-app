61448afe288a52ee5628dc9bbd184faf73bc76d8
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-05-27 23:41:41 +0300

    feat: create isAuthenticated function

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import '../styles/HomeLayout.css';
+import { isAuthenticated } from '../utils/auth';
 
 const HomeLayout = () => {
+  const isLoggedIn = isAuthenticated();
+
   return (
     <>
       <header className="header">
@@ -15,9 +18,15 @@ const HomeLayout = () => {
             <li className="header__nav-item">
               <Link to="about" className='header__nav-link'>About</Link>
             </li>
-            <li className="header__nav-item">
-              <Link to="account" className='header__nav-link'>Account</Link>
-            </li>
+            {isLoggedIn ? (
+              <li className="header__nav-item">
+                <Link to="account" className='header__nav-link'>Account</Link>
+              </li>
+            ) : (
+              <li className="header__nav-item">
+                <Link to="login" className='header__nav-link'>Login</Link>
+              </li>
+            )}
             <li className="header__nav-item">
               <Link to="admin" className='header__nav-link'>Admin</Link>
             </li>
