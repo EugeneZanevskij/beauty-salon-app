68869f01fc4b8cafd67f4fd85182d1cde4e9b1cd
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-05-29 00:24:07 +0300

    feat: HomeLayout and index.css

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -5,30 +5,33 @@ import { isAuthenticated } from '../utils/auth';
 
 const HomeLayout = () => {
   const isLoggedIn = isAuthenticated();
+  //TODO: idAdmin
 
   return (
     <>
       <header className="header">
+      <Link to="" className="header__link">
         <h1 className="header__title">Z&Z Beauty Lounge</h1>
+      </Link>
         <nav className="header__nav">
           <ul className="header__nav-list">
             <li className="header__nav-item">
-              <Link to="" className="header__nav-link">Main</Link>
+              <Link to="" className="header__link">Main</Link>
             </li>
             <li className="header__nav-item">
-              <Link to="about" className='header__nav-link'>About</Link>
+              <Link to="about" className='header__link'>About</Link>
             </li>
             {isLoggedIn ? (
               <li className="header__nav-item">
-                <Link to="account" className='header__nav-link'>Account</Link>
+                <Link to="account" className='header__link'>Account</Link>
               </li>
             ) : (
               <li className="header__nav-item">
-                <Link to="login" className='header__nav-link'>Login</Link>
+                <Link to="login" className='header__link'>Login</Link>
               </li>
             )}
             <li className="header__nav-item">
-              <Link to="admin" className='header__nav-link'>Admin</Link>
+              <Link to="admin" className='header__link'>Admin</Link>
             </li>
           </ul>
         </nav>
