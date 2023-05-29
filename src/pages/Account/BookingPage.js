40dc993d16ca57081d328fcd61ffa6cb15b61547
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-05-29 03:28:51 +0300

    feat: update BookingPage and its css

--- src/pages/Account/BookingPage.js
+++ src/pages/Account/BookingPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BookingForm } from './BookingForm';
+import '../../styles/BookingPage.css';
 
 const BookingPage = () => {
   return (
-    <div>
-      <h2>Book Appointment</h2>
+    <div className="booking-page">
+      <h1 className="booking-page__title">Book An Appointment</h1>
+      <h3 className="booking-page__subtitle">Here you can book an appointment</h3>
       <BookingForm />
+      <p className="booking-page__paragraph">Click on the button <span className='booking-page__span'>twice</span> to book an appointment</p>
     </div>
   );
 };
